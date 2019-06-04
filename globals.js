00365c507ae2231b5a75de6e799aa9a3d7829f06
Ganesh Ram <ganeshram997@gmail.com>
2019-06-04 16:22:06 -0400

    globasl.js

--- globals.js
+++ globals.js
@@ -1,12 +1,10 @@
 module.exports = {};
-/*
-	Local Testing
-*/
-module.exports.SERVER_URI_PREFIX = "http://localhost:3000";
-module.exports.SERVER_PORT = 3000; 
 
-/*
-	Ngrok Testing
-*/
-
-//module.exports.SERVER_URI_PREFIX = "https://9234747b.ngrok.io"; 
+testing=true;
+if (testing) {
+	module.exports.SERVER_URI_PREFIX = "http://localhost:3000";
+}
+else{
+	module.exports.SERVER_URI_PREFIX = "http://localhost:3000";
+}
+module.exports.SERVER_PORT = 3000;
